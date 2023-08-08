9ef5bd09c76109b095c05b22f51f6c2099d4ddac
rishi.agrawal@corestack.io <rishi@ubuntu-ws.ubuntu.rishi.com>
2023-08-08 14:39:37 +0530

    debug

--- Azure/AZURE_POWERSHELL_LOCAL/main.tf
+++ Azure/AZURE_POWERSHELL_LOCAL/main.tf
@@ -1,9 +1,23 @@
-variable "name" { }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "East US"
+}
 
-resource "null_resource" "script1" {
+resource "null_resource" "execute_powershell" {
   provisioner "local-exec" {
-    command = "Write-Host \"Hello ${var.name}\""
-
+    command = <<-EOT
+      $randomValue = Get-Random -Minimum 100 -Maximum 1000
+      $randomValue | Out-File random_output.txt
+    EOT
     interpreter = ["PowerShell", "-Command"]
+    working_dir = path.module
   }
+
+  depends_on = [
+    azurerm_resource_group.example,
+  ]
+}
+
+output "random_value_output" {
+  value = file("random_output.txt")
 }
