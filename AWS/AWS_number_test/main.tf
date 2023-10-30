f64623339d659da184417f6ef9019ee479933775
Rishi Agrawal <Rishi.Agrawal@corestack.io>
2023-10-30 12:08:15 +0530

    with-constraints

--- AWS/AWS_number_test/main.tf
+++ AWS/AWS_number_test/main.tf
@@ -1,6 +1,10 @@
 variable "number_test" {
   type = number
   default = 600
+  validation {
+    condition = 0 >=  var.number_test >= 500
+    error_message = "The number must be greater than 500"
+  }
 }
 
 output "test" {
